76220bbb2bc85668ab7c401d502f79528ef210dc
Sieberti50 <pedalek34@gmail.com>
2025-10-06 09:27:50 +0200

    server cz.2

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const port = 3000;
 
 const counterFile = 'counter.txt';
+const guestsFile = 'guests.txt';
 
-// Odczytaj licznik z pliku counter.txt lub ustaw na 0
 let counter = 0;
 if (fs.existsSync(counterFile)) {
   try {
@@ -15,11 +15,38 @@ if (fs.existsSync(counterFile)) {
 }
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
+  if (url.pathname === '/') {
     counter++;
     fs.writeFileSync(counterFile, counter.toString());
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(`Witaj na stronie! Odwiedziłeś ją już ${counter} razy.`);
+  } else if (url.pathname === '/add') {
+    const name = url.searchParams.get('name');
+    if (!name) {
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.end('Błąd: brak parametru name.');
+      return;
+    }
+    fs.appendFile(guestsFile, name + '\n', (err) => {
+      if (err) {
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Błąd zapisu.');
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(`Dodano ${name} do listy gości.`);
+      }
+    });
+  } else if (url.pathname === '/list') {
+    fs.readFile(guestsFile, 'utf8', (err, data) => {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      if (err || !data) {
+        res.end('Lista gości jest pusta.');
+      } else {
+        res.end('Lista gości:\n' + data);
+      }
+    });
   } else {
     res.writeHead(404, {'Content-Type': 'text/plain'});
     res.end('404 - Strona nie istnieje');
